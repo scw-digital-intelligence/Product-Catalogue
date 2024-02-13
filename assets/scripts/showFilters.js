892403d70e1ef697788f4ecfa800a682413d26e7
Jeffrey Morgan-Harrisskitt <44020197+jeff-harrisskitt@users.noreply.github.com>
2024-02-13 09:38:25 +0000

    async issue with showing filters resolved

--- assets/scripts/showFilters.js
+++ assets/scripts/showFilters.js
@@ -1,20 +1,17 @@
-/* shows or hides the product filters */
-window.onpageshow = function(){
-    showFilters();
-}
-
 // identifies the current page
 let path = window.location.pathname;
 let page = path.split("/").pop();
 
-// stores the product filter element
-let filters = document.getElementById("filter-container")
-
 // changes diplay property based on current page
 function showFilters(){
-    if (page == "catalogue.html"){
-        filters.style.display = "flex"
-    } else {
-        filters.style.display = "none"
-    }
+    setTimeout(function(){
+        // stores the product filter element
+        let filters = document.getElementById("filter-container")
+
+        if (page == "catalogue.html"){
+            filters.style.display = "flex"
+        } else {
+            filters.style.display = "none"
+        }
+    }, 1);
 }
