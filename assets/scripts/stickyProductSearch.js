20b708b349b0af436d968a69f6b9fec171bb1ce0
Jeffrey Morgan-Harrisskitt <44020197+jeff-harrisskitt@users.noreply.github.com>
2024-02-23 15:23:31 +0000

    product search list added

--- assets/scripts/stickyProductSearch.js
+++ assets/scripts/stickyProductSearch.js
@@ -13,4 +13,68 @@ function productSticky() {
     }
 }
 
+productDistinct.forEach(function (product){
+    let list = document.getElementById("product-search-list")
 
+    let new_item = document.createElement("li");
+    let new_link = document.createElement("a");
+    new_link.textContent = product.Report_Title;
+
+    new_item.appendChild(new_link);
+    list.appendChild(new_item);
+
+});
+
+// Script to search portfolio array for products and return results
+
+function productSearchResults() {
+    let input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("product-search-bar");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("product-search-list");
+    li = ul.getElementsByTagName("li");
+    console.log(input);
+    console.log(filter);
+    console.log(ul);
+    console.log(li);
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+
+    if(filter.length >= 1){
+        ul.classList.remove("hide-search-results")
+    } else {
+        ul.classList.add("hide-search-results")
+    }
+}
+
+// let prodSearchTerm = window.setInterval(function(){
+//     prodSearch.childNodes[1].value
+// }, 100);
+
+// window.setInterval(function(){
+//     if (prodSearchTerm != '') {
+//         portfolios.forEach(function(portfolio) {
+//             let prods = portfolio.Products
+//             for (let key in prods) {
+//                 if (typeof prods[key] === "object") {
+//                     for (let nestedKey in prods[key]) {
+//                         if (prods[key][nestedKey] == prodSearchTerm){
+//                             console.log(prods[key].Name)
+//                             return prods[key]
+//                         }
+//                         //console.log(prods[key][nestedKey])       
+//                     }
+//                 } else {
+//                     console.log(prods[key]);
+//                 }
+//             }
+//         });
+//     }
+// }, 100);  
