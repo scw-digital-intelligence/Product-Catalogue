3e2b8eb05f9e9d53c1ba7b504cba1b2bec674a95
Jeffrey Morgan-Harrisskitt <jeffrey.harrisskitt@scwcsu.nhs.uk>
2024-03-05 13:27:13 +0000

    Increased speed of product page load by creating elements instead of replacing

--- assets/scripts/productDeets.js
+++ assets/scripts/productDeets.js
@@ -1,21 +1,43 @@
+const product = JSON.parse(localStorage.getItem("useThisProduct") || "[]");
+
 // function to update element properties to match clicked product
 function makeProduct(){
-    // update banner text
-    bannerText = document.getElementById("product-banner-text")
-    bannerText.textContent = `${product.Name}`
+    // declaring required variables
+    let bannerText, bannerImage, titleDesc, linkedinURL, twitterURL;
+    let bannerTextParent, titleDescParent, carouselInner;
+
+    // add banner text
+    bannerText = document.createElement("h1");
+    bannerText.setAttribute("id","product-banner-text");
+    bannerText.textContent = `${product.Name}`;
+
+    bannerTextParent = document.getElementById("hero-content-product");
+    bannerTextParent.appendChild(bannerText);
 
     // update banner image
-    bannerImage = document.getElementById("hero-product")
-    bannerImage.style.backgroundImage =  `linear-gradient(rgba(0, 0, 0, 0.392), rgba(0, 0, 0, 0.392)), url(${product.Image})`
+    bannerImage = document.getElementById("hero-product");
+    bannerImage.style.backgroundImage =  `linear-gradient(rgba(0, 0, 0, 0.392), rgba(0, 0, 0, 0.392)), url(${product.Image})`;
 
-    // update product description
-    titleDesc = document.getElementById("product-desc")
-    titleDesc.textContent = `${product.Description}`
+    // adding product description
+    titleDesc = document.createElement("p");
+    titleDesc.setAttribute("id","product-desc");
+    titleDesc.textContent = `${product.Description}`;
 
-    // updating links for sharing on LinkedIn and Twitter
-    linkedinURL = document.getElementById("linkedin-btn")
-    linkedinURL.setAttribute("href", `https://www.linkedin.com/sharing/share-offsite/?url=${window.location.href}`)     
+    titleDescParent = document.getElementById("product-title-container");
+    titleDescParent.appendChild(titleDesc);
+
+    // adding links for sharing on LinkedIn and Twitter
+    linkParams = product.Name.replace(/\s+/g, "-").toLowerCase();
+
+    linkedinURL = document.getElementById("linkedin-btn");
+    linkedinURL.setAttribute("href", `https://www.linkedin.com/sharing/share-offsite/?url=${window.location.href}&${linkParams}`)  ;   
 
     twitterURL = document.getElementById("twitter-btn")
-    twitterURL.setAttribute("href", `http://twitter.com/share?text=${product.Description}&url=${window.location.href}&hashtags=NHS,SCWCSU,HealthcareAnalytics`) 
+    twitterURL.setAttribute("href", `http://twitter.com/share?text=${product.Description}&url=${window.location.href}&${linkParams}&hashtags=NHS,SCWCSU,HealthcareAnalytics`);
+
+    // contextually adding carousel
+    carouselInner = [].slice.call(document.getElementById("carousel-inner").getElementsByTagName('img'),0);
+    carouselInner[0].setAttribute("src",`${product.Carousel_Images_1}.svg`)
+    carouselInner[1].setAttribute("src",`${product.Carousel_Images_2}.svg`)
+    carouselInner[2].setAttribute("src",`${product.Dummy_Product_URL}.svg`)  
 }
