112725d8c62f7f2ff5830e863f472aca0948209c
Jeffrey Morgan-Harrisskitt <jeffrey.harrisskitt@scwcsu.nhs.uk>
2024-03-14 09:21:50 +0000

    settings.py added to gitignore

--- assets/scripts/settings.py
+++
@@ -1,5 +0,0 @@
-USER_CRED = "jeffrey.harrisskitt@scwcsu.nhs.uk"
-USER_PASSWORD = "harri0600155"
-SERVER = r'BIS-000-SP08.bis.xswhealth.nhs.uk, 14431'
-DATABASE = 'DigitalIntelligence'
-SHAREPOINT = "https://scwcsu.sharepoint.com/sites/"
