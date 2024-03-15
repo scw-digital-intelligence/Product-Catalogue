563a4336d59b96e22ad955b200dec73353ae9f0b
Jeffrey Morgan-Harrisskitt <jeffrey.harrisskitt@scwcsu.nhs.uk>
2024-03-15 18:27:10 +0000

    catalogue box scrolling adapted for screen sizes

--- assets/scripts/catalogueScroll.js
+++ assets/scripts/catalogueScroll.js
@@ -7,8 +7,18 @@ function addScrollArrows(){
         let arrow = chevronsRight[i]
         // function to scroll the catalogue elements on click
         arrow.onmousedown = function(){
-            let scrollAmount = this.parentElement.parentElement.scrollWidth;
-            this.parentElement.previousSibling.scrollLeft += (scrollAmount * 0.35);
+            // let scrollAmount = this.parentElement.parentElement.scrollWidth;
+            // this.parentElement.previousSibling.scrollLeft += (scrollAmount * 0.35);
+            let scrollAmount = this.parentElement.parentElement.querySelector('.content-box').getBoundingClientRect().width;
+            
+            if(scrollAmount > 300){
+                this.parentElement.previousSibling.scrollLeft += (scrollAmount + (scrollAmount*0.22));
+            } else if(scrollAmount > 250) {
+                this.parentElement.previousSibling.scrollLeft += (scrollAmount + (scrollAmount*0.14));
+            } else {
+                this.parentElement.previousSibling.scrollLeft += (scrollAmount + (scrollAmount*0.0786));
+            }
+            
         }
     }
 
@@ -16,8 +26,17 @@ function addScrollArrows(){
         let arrow = chevronsLeft[i]
         // function to scroll the catalogue elements on click
         arrow.onmousedown = function(){
-            let scrollAmount = this.parentElement.parentElement.scrollWidth;
-            this.parentElement.nextSibling.scrollLeft -= (scrollAmount * 0.35);
+            // let scrollAmount = this.parentElement.parentElement.scrollWidth;
+            // this.parentElement.nextSibling.scrollLeft -= (scrollAmount * 0.35);
+            let scrollAmount = this.parentElement.parentElement.querySelector('.content-box').getBoundingClientRect().width;;            
+            
+            if(scrollAmount > 300){
+                this.parentElement.nextSibling.scrollLeft -= (scrollAmount + (scrollAmount*0.22));
+            } else if(scrollAmount > 250) {
+                this.parentElement.nextSibling.scrollLeft -= (scrollAmount + (scrollAmount*0.14));
+            } else {
+                this.parentElement.nextSibling.scrollLeft -= (scrollAmount + (scrollAmount*0.0786));
+            }
         }
     }    
 }
